c80d5df49e8f2e8fcf693fab4f68ecbda186c14e
Rijaniaina Elie Fidèle <elierijaniaina@gmail.com>
2021-09-09 12:54:23 +0300

    nouveau commit du projet

--- src/app/utils/urls.ts
+++ src/app/utils/urls.ts
@@ -2,13 +2,13 @@ export class Urls {
   private BACK_END_PROTOCOL = 'http://';
   private BACK_END_SERVER_HOST = 'localhost';
   private BACK_END_SERVER_PORT = 8080;
-  private BACK_END_NAMESPACE = '/vente-de-terrain/';
+  private BACK_END_NAMESPACE = '/';
 
   private IMAGE_SERVER_PROTOCOL = 'http://';
   private IMAGE_SERVER_HOST = 'localhost';
   private IMAGE_SERVER_PORT = 80;
-  private PROFILE_SERVER_NAMESPACE = '/vente-de-terrain/images/clients/';
-  private FIELD_SERVER_NAMESPACE = '/vente-de-terrain/images/terrains/';
+  private PROFILE_SERVER_NAMESPACE = '/localstorage/vente-de-terrain/images/clients/';
+  private FIELD_SERVER_NAMESPACE = '/localstorage/vente-de-terrain/images/terrains/';
 
   getBackendUrl(): string {
     return this.BACK_END_PROTOCOL + this.BACK_END_SERVER_HOST + ':' + this.BACK_END_SERVER_PORT + this.BACK_END_NAMESPACE;
